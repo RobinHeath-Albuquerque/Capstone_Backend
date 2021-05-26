f3bebc989e8b068d2cbab4995f8b5621ea7b6016
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-05-26 09:52:28 -0600

    Adding in Registration templates with login, sign up and logged out html pages.

--- FarmersMarket/farmersMarket/market/urls.py
+++ FarmersMarket/farmersMarket/market/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
+from django.contrib import admin
 
 app_name = 'market'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('new/', views.create, name='create_new_market')
+    path('', views.index, name='home'),
+    path('registration/sign_up/', views.sign_up, name="sign_up")
 ]
--- FarmersMarket/farmersMarket/market/views.py
+++ FarmersMarket/farmersMarket/market/views.py
@@ -1,23 +1,52 @@
+from pickle import NONE
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Market, Employee, Grower, Buyer
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
+
+@login_required()
 def index(request):
-    all_markets = Market.objects.all()
-    context = {
-        'all_markets': all_markets
-    }
-    return render(request, 'market/index.html', context)
+    return render(request, 'registration/index.html')
 
 
-def create(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        time = request.POST.get('time')
-        location = request.POST.get('location')
-        new_market = Market(name=name, time=time, location=location)
-        new_market.save()
-        return HttpResponseRedirect(reverse('market:index'))
-    else:
-        return render(request, 'market/create.html')
+def sign_up(request):
+    context = {}
+    form = UserCreationForm(request.POST or NONE)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return render(request, 'registration/index.html')
+        context['form'] = form
+        return render(request, 'registration/sign_up.html', context)
+
+
+#def index(request):
+    #all_markets = Market.objects.all()
+
+    #context = {
+       # 'all_markets': all_markets
+   # }
+   # return render(request, 'market/index.html', context)
+
+
+#def create(request):
+
+   # if request.method == 'POST':
+        #name = request.POST.get('name')
+        #time = request.POST.get('time')
+        #location = request.POST.get('location')
+        #new_market = Market(name=name, time=time, location=location)
+        #new_market.save()
+       # return HttpResponseRedirect(reverse('market:index'))
+    #else:
+        #return render(request, 'market/create.html'
+
+
+
