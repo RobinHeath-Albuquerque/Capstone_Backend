9403287bbd241045704f3e2ddd05afba6f41f789
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-06-07 17:19:09 -0600

    Adding in new back end files

--- Capstone/farmersMarket/markets/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Market, ProductCategory, Product, Shopper, Grower
-
-admin.site.register(Market)
-admin.site.register(Product)
-admin.site.register(ProductCategory)
-admin.site.register(Shopper)
-admin.site.register(Grower)
--- Capstone/farmersMarket/markets/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'markets'
-urlpatterns = [
-    path('', views.index, name ='index'),
-    path('new/', views.create, name='create_new_grower')
-]
